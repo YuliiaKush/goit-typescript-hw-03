2ae6040ef2a028ac8fa888ac21c889e97443dd6c
YuliiaKush <128642386+YuliiaKush@users.noreply.github.com>
2023-11-24 10:41:33 +0200

    final updatee

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,52 @@
+class Key {
+  constructor(private signature: number = Math.random()) {}
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door = false;
+  protected tenants: Person[] = [];
+  protected key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+abstract openDoor(key: Key): void;
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Door is open. Person enters the house.");
+    } else {
+      console.log("Door is closed. First, you must open the door.");
+    }
+  }
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (this.key.getSignature() === key.getSignature()) {
+      this.door = true;
+      console.log("Door is open.");
+    } else {
+      console.log("Door is closed.");
+    }
+  }
+}
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
